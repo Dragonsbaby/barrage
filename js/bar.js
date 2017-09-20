a3abd928eb986a7aaec59b04c0db8578cecf8b77
DragonBaby <854152562@qq.com>
2017-09-20 18:13:32 +0800

    finished

--- js/bar.js
+++ js/bar.js
@@ -4,53 +4,132 @@
 
 
 (function(win){
+
+    var _class_ = 'barrage-content';
+    var _maxWidth_ = document.body.clientWidth;
+
+
     var BA = function () {
         this.elem = null;
+        this.text = '';
         this.right = 0;
         this.top = 0;
+        this.color = '';
+        this.translateZ = 0;
         this.speed = 1;
     };
 
     BA.prototype = {
-        creatNew : function (text) {
-
+        createNew : function (text,speed) {
+            this.elem = P.CreateElem();
+            this.text = text;
+            this.top = P.random.top();
+            this.color = P.random.color();
+            this.speed = speed;
+            this.translateZ = P.random.translateZ();
+            this.setElem();
+            this.stay_move();
+            P.insert(this.elem);
         },
         randomNew : function () {
-            this.elem =
+            this.elem = P.CreateElem();
+            this.text =(new ChinsesCart()).init();
+            this.top = P.random.top();
+            this.color = P.random.color();
+            this.speed = P.random.speed();
+            this.translateZ = P.random.translateZ();
+            this.setElem();
+            this.stay_move();
+            P.insert(this.elem);
+        },
+        setElem : function () {
+            this.elem.innerHTML = this.text;
+            this.elem.className = _class_;
+            this.elem.style.top = this.top + 'px';
+            this.elem.style.right = this.right + 'px';
+            this.elem.style.color = this.color;
+            this.elem.style.webkitTransform = 'translateZ('+this.translateZ+'px)';
+
+        },
+        stay_move : function () {
+            var _this = this;
+            flashMap(60,function (timer) {
+                if(typeof(_this) == 'undefined'||_this == null || _this == '') {return;}
+                var speed = (_this.speed / 60).toFixed(2);
+                var right = (_this.elem.style.right).replace("px","");
+                if(right >= _maxWidth_){
+                    _this.elem.parentNode.removeChild(_this.elem);
+                    _this = null;
+                    clearInterval(timer);
+                    return ;
+                }
+                var add = parseFloat(right) + parseFloat(speed);
+                _this.elem.style.right = add + 'px';
+            })
         }
+    };
+
+    BA.setWindowWidth = function(width){
+        _maxWidth_ = width;
     };
 
     var P = {};
     P.random = {
         top : function () {
-
+            return Math.floor(Math.random()*355)+5;
         },
         speed : function () {
-            return Math.floor(Math.random()*20)+10;
+            return Math.floor(Math.random()*120)+120;
+        },
+        translateZ : function () {
+            return Math.floor(Math.random()*100);
+        },color : function () {
+            return '#' + (function (color) {
+                    return new Array(7 - color.length).join('0')+color;
+                })((Math.random()*0x1000000 << 0).toString(16));
         }
     };
-    P.insert = function () {
-        var elem = document.createElement()
+    P.CreateElem = function () {
+        return document.createElement('div');
+    };
+
+    P.insert = function (elem) {
+        var wrap = document.getElementById('BA-wrap');
+        wrap.appendChild(elem);
+    };
+
+    /**
+     * 能够修复因js定时器带来的延时差
+     * @param fps  每秒多少帧  推荐60
+     * @param callback  每次刷新执行的函数
+     */
+    var flashMap = function (fps,callback) {
+        var prev = new Date();
+        var acc = 0;
+        var dt = 1000/fps;
+
+        var loop = function () {
+            var current = new Date();
+            var passed = current - prev;
+            prev = current;
+            acc += passed;
+            while(acc >= dt){
+                if(typeof callback == 'function'){
+                    callback(timer);
+                }
+                acc -= dt;
+            }
+        };
+        var timer = setInterval(loop,1000/fps);
     };
 
 
     win.barrage = BA;
-})(window)
+})(window);
 
 (function(wind){
 
 
-
-    var BA = function(){
-
-    };
-
-
-    BA.prototype = {
-        init : function () {
-
-        }
-    };
 
     var CC = function(){
     };
@@ -60,8 +139,6 @@
         init : function () {
             var num = this.randomNum();
             var cc = this.randomCC(num);
-            console.log(getRandomColor());
-            console.info("num", num);
             return  cc;
         },
         randomNum : function () {
@@ -77,11 +154,6 @@
         }
     };
 
-    var getRandomColor = function () {
-        return '#'+(function (color) {
-                return new Array(7-color.length).join("0")+color;
-            })((Math.random()*0x1000000 << 0).toString(16));
-    };
 
 
     window.ChinsesCart = CC;
@@ -89,7 +161,57 @@
 
 
 
+$(function () {
+    delayCreateBarrage(1.5,function () {
+        new barrage().randomNew();
+    });
+
+    $(".emit-btn").on("click",function(){
+        var input =  $(this).parent().find(".emit-input");
+        var checkedId = $(this).parent().find(".emit-radio:checked").attr("id");
+        emitBarrage(input, checkedId);
+    });
+    $(".emit-input").on("keydown",function (e) {
+        var key = e.keyCode ? e.keyCode : e.which;
+        if (key == 13) {
+            var input = $(this).parent().find(".emit-input");
+            var checkedId = $(this).parent().find(".emit-radio:checked").attr("id");
+            emitBarrage(input, checkedId)
+        }
+    });
+
+    $(window).resize(function () {
+        barrage.setWindowWidth(document.body.clientWidth)
+    });
+});
+
+var emitBarrage = function (input,checkedId) {
+    var speed = 0;
+    switch(checkedId){
+        case "emit-slow":
+            speed = 0+120;
+            break;
+        case "emit-normal":
+            speed = 60+120;
+            break;
+        case "emit-fast":
+            speed = 120+120;
+            break;
+    }
+    if(input.val() == '') {return false;}
+    new barrage().createNew(input.val(),speed);
+    input.val('');
+};
 
 
+var delayCreateBarrage = function (s,callback) {
+    var timer = setTimeout(function () {
+        if(typeof callback == 'function'){
+            callback();
+            clearTimeout(timer);
+        }
+        delayCreateBarrage(s,callback);
+    },s*1000);
 
+};
 
